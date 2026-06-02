8760c7b580ff2cd80c91999943dd6f9fdf3fb577
Ayoubito04 <ayoubarramdani091@gmail.com>
2026-06-03 00:29:59 +0200

    Hemos terminado la parte de FAQ

--- proyecto2_react/src/pages/FAQ/FAQ.jsx
+++ proyecto2_react/src/pages/FAQ/FAQ.jsx
@@ -1,4 +1,3 @@
-//Vamos a empezar con el componente de FAQ
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
@@ -9,9 +8,10 @@ import "./FAQ.css";
 const FAQ=()=>{
     //Primero vamos a definir un estado que se encarge de mostrar cada una de las repuestas de las preguntas frecuentes,para esto vamos a usar el hook de UseState,que nos permite definir un estado dentro de un componente funcional,este estado puede ser cualquier tipo de dato,como un string,un numero,un array o un objeto
     const [isOpen,setIsOpen] = useState(false);
-
-    const handleFAQ=()=>{
-        setIsOpen(!isOpen);
+        
+    const handleFAQ=(index)=>{
+        setIsOpen(isOpen===index?null:index);
+        //Esto nos permite abrir y cerrar cada una de las repuestas de las preguntas frecuentes,cuando el estado es igual al index de la pregunta frecuente,mostramos la repuesta,cuando el estado es diferente al index de la pregunta frecuente,ocultamos la repuesta
         //Aquí vamos a incluir la lógica para mostrar cada una de las repuestas de las preguntas frecuentes,para esto vamos a usar el estado que hemos definido anteriormente,que se encarga de mostrar cada una de las repuestas de las preguntas frecuentes,cuando el estado es true,mostramos la repuesta,cuando el estado es false,ocultamos la repuesta
     }
    //Dentro del componente la lógica es simple,tenemos que definir que tiene que hacer cierto componente en cierto estado,los Usestate es para poder renderizar o transformar cada uno de los vcompoinentes usando condicionales
@@ -21,18 +21,29 @@ const FAQ=()=>{
         <main className="faq-main">
             <h1>Preguntas Frecuentes</h1>
             <div className="faq-container">
-                <div className="faq-item">
-                    <h2 onClick={handleFAQ}>¿Por qué usar GoFight antes que otras opciones y qué es lo que destacarías de Go?</h2>
-                    {isOpen && <p>Puedes contactarnos a través de nuestro formulario de contacto,que se encuentra en la sección de contacto de nuestro sitio web, o también puedes enviarnos un correo electrónico a la dirección de correo electrónico que se encuentra en la sección de contacto de nuestro sitio web.</p>}
+                <div className={`faq-item${isOpen === 0 ? ' faq-item--open' : ''}`}>
+                    <h2 onClick={() => handleFAQ(0)}>¿Por qué usar GoFight antes que otras opciones y qué es lo que destacarías de Go?</h2>
+                    {isOpen === 0 && <p>Por qué GoFight se enfoca en un nicho en especifico como el boxeo y también logra atraer al público más joven.</p>}
                 </div>
-                <div className="faq-item">
-                    <h2 onClick={handleFAQ}>¿Qué servicios ofrece GoFight?</h2>
-                    {isOpen && <p>GoFight ofrece una amplia gama de servicios relacionados con el desarrollo web,como diseño web,desarrollo web,marketing digital,SEO,SEM,redes sociales y
-mucho más.Para obtener más información sobre nuestros servicios,puedes visitar la sección de servicios de nuestro sitio web.</p>}
+                <div className={`faq-item${isOpen === 1 ? ' faq-item--open' : ''}`}>
+                    <h2 onClick={() => handleFAQ(1)}>¿De cuantas tablas consta la BD de GoFight?</h2>
+                    {isOpen === 1 && <p>La base de datos de GoFight consta de 5 tablas, incluyendo tablas como la de usuarios,gamificaciones,sesiones,rutinas,ejercicios o también tablas intermedias de N:M como la de rutinas_usuarios,donde se registraba el tiempo de duración y descanso de cada uno de los ejercicios.</p>}
                 </div>
-                <div className="faq-item">
-                    <h2 onClick={handleFAQ}>¿Cuánto cuesta contratar los servicios de GoFight?</h2>
-                    {isOpen && <p>El costo de contratar los servicios de GoFight depende del tipo de servicio que deseas contratar,del alcance del proyecto y de otros factores.Para obtener una cotización personalizada,puedes contactarnos a través de nuestro formulario de contacto o enviarnos un correo electrónico a la dirección de correo electrónico que se encuentra en la sección de contacto de nuestro sitio web.</p>}
+                <div className={`faq-item${isOpen === 2 ? ' faq-item--open' : ''}`}>
+                    <h2 onClick={() => handleFAQ(2)}>¿Cual es vuestro plan a futuro?</h2>
+                    {isOpen === 2 && <p>Ampliar nuestro nicho a más artes marciales,ya sea Teakwondo, Judo o Karate, y seguir mejorando nuestra plataforma para ofrecer la mejor experiencia a nuestros usuarios.</p>}
+                </div>
+                  <div className={`faq-item${isOpen === 3 ? ' faq-item--open' : ''}`}>
+                    <h2 onClick={() => handleFAQ(3)}>¿Qué es lo que más destaca de vuestro proyecto?</h2>
+                    {isOpen === 3 && <p>Lo que más destaca de nuestro proyecto es nuestra dedicación a ofrecer una experiencia única a la hora de aparender a bpxear,ya que por el momento no existe como tal una app,que cuente cuente con varias rutinas y ejercicios de distintas categorias</p>}
+                </div>
+                   <div className={`faq-item${isOpen === 4 ? ' faq-item--open' : ''}`}>
+                    <h2 onClick={() => handleFAQ(4)}>¿Qué es lo que más destaca de vuestro proyecto?</h2>
+                    {isOpen === 4 && <p>Lo que más destaca de nuestro proyecto es nuestra dedicación a ofrecer una experiencia única a la hora de aparender a bpxear,ya que por el momento no existe como tal una app,que cuente cuente con varias rutinas y ejercicios de distintas categorias</p>}
+                </div>
+                   <div className={`faq-item${isOpen === 5 ? ' faq-item--open' : ''}`}>
+                    <h2 onClick={() => handleFAQ(5)}>¿De qué se ha encargado cada uno?</h2>
+                    {isOpen === 5 && <p>Cada uno se ha encargado de diferentes partes cruciales de la app.Ayoub se ha encargado del Backend y la consulatas API,Agustin se ha encargado de diferentes de aspectos comola documentación y de diferentes pantallas de frontend,ya se la de perfil de usuario y la de progreso.Mario se ha encargado de la parte del diseño de la app,dandole ese toque de rojo neon con una tonalidad oscura.</p>}
                 </div>
             </div>
         </main>
@@ -40,4 +51,5 @@ mucho más.Para obtener más información sobre nuestros servicios,puedes visita
         </>
     )
 }
+
 export default FAQ;
